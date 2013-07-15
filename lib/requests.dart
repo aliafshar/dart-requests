10bcff5bba09b9dac7a3cc52877fa3b2323678bc
Ali <afshar@google.com>
2013-07-14 19:34:52 -0700

    Update to latest APIs.

--- lib/requests.dart
+++ lib/requests.dart
@@ -1,251 +1,205 @@
+library requests;
 
 import 'dart:io';
-import 'dart:uri';
-import 'dart:json';
+import 'dart:json' as JSON;
+import 'dart:async';
+import 'package:logging/logging.dart';
 
+Logger LOG = new Logger('requests');
 
+class ValueNotPassedSentinel {
 
-abstract class Data {
-  ContentType contentType;
-  InputStream stream;
-  int contentLength;
 }
 
-
-class StringData implements Data {
-
-  final ContentType contentType;
-  final InputStream stream = new ListInputStream();
-  int contentLength;
-
-  StringData(String data, this.contentType, [encoding = Encoding.UTF_8]) {
-    ListInputStream s = stream;
-    s.write(data.charCodes);
-    contentLength = data.charCodes.length;
+class Content {
+  ContentType type;
+  Stream<List<int>> data;
+  int length;
+  
+  Content();
+  
+  factory Content.forString(String value, type, [encoding = Encoding.UTF_8]) {
+    Content c = new Content();
+    c.type = type;
+    final encoder = new StringEncoder();
+    c.data = encoder.bind
+      (new Stream<String>.fromIterable
+        ([value]));
   }
 
-}
-
-
-class JsonData extends StringData {
-
-  JsonData(dynamic data) :
-    super( JSON.stringify(data)
-         , new ContentType.fromString('application/json'));
-}
-
-
-class StreamData implements Data {
-
-  final InputStream stream;
-  final ContentType contentType;
-
-  StreamData(this.stream, this.contentType);
-
+  factory Content.forJson(dynamic obj) {
+    return new Content.forString
+      ( JSON.stringify(data)
+      , ContentType.parse('application/json'));
+  }
+  
+  write(HttpClientRequest req) {
+    req.addStream(data);
+    req.headers.set(HttpHeaders.CONNECTION, 'close');
+    req.headers.set(HttpHeaders.CONTENT_LENGTH, data.length);
+    req.headers.set(HttpHeaders.TRANSFER_ENCODING, 'chunked');
+    req.headers.set(HttpHeaders.CONTENT_TYPE, type.toString());
+  }
 }
 
 
 class Request {
 
-  String method;
-  String url;
-  Data data;
-  List<Cookie> cookies;
-  Map<String, String> params;
-  Map<String, String> headers;
-  bool stream;
-  List<String> parts;
-  List<InputStream> partStreams;
-
   HttpClient client = new HttpClient();
-  HttpClientRequest raw;
+  String method;
+  Uri uri;
+  Content data = new Content();
+  List<Cookie> cookies = <Cookie>[];
+  Map<String, String> headers = <String, String>{};
+  Map<String, String> params = <String, String>{};
 
-  Response response = new Response();
-
-  Completer<Response> finished = new Completer<Response>();
-
-  Request
-    ( this.method
-    , this.url
-    ,
-    { Map<String, String> params
-    , this.data
-    , Map<String, String> headers
-    , List<Cookie> cookies
-    , List<String> parts
-    , List<InputStream> partStreams
-    , this.stream : false
-    }) {
-    this.params = (params != null) ? params : <String>{};
-    this.headers = (headers != null) ? headers : <String>{};
-    this.cookies = (cookies != null) ? cookies : <Cookie>[];
-    this.parts = (parts != null) ? parts : <String>[];
-    this.partStreams = (partStreams != null) ? partStreams : <InputStream>[];
+  Uri get actualUri {
+    return mergeArgs(uri, params);
   }
 
-  Uri get actualUrl {
-    return mergeArgs(new Uri(url), params);
-  }
-
-  Future<Response> execute() {
-    var conn = client.openUrl(method, actualUrl);
-    conn.onError = _onError;
-    conn.onResponse = _onResponse;
-    conn.onRequest = _onRequest;
-    response._finished.future.then((response) {
-      finished.complete(response);
-      client.shutdown();
-    });
-    return finished.future;
-  }
-
-  _onRequest(HttpClientRequest raw) {
-    this.raw = raw;
+  prepareRequest(HttpClientRequest req) {
     if (data != null) {
-      raw.headers.set(HttpHeaders.CONNECTION, 'close');
-      raw.headers.set(HttpHeaders.CONTENT_LENGTH, data.contentLength);
-      raw.headers.set(HttpHeaders.TRANSFER_ENCODING, 'chunked');
-      raw.headers.set(HttpHeaders.CONTENT_TYPE, data.contentType.toString());
-      data.stream.pipe(raw.outputStream);
-      //raw.outputStream.write(data.stream.read());
-    } else {
-      raw.outputStream.close();
+      data.write(req);
     }
+    return req.close();
   }
 
-  _onClosed() {
-    print('closed');
+  Future<Response> onResponse(HttpClientResponse raw) {
+    final resp = new Response(raw);
+    return resp.finished.future;
   }
-
-  _onResponse(HttpClientResponse raw) {
-    response._bind(this, raw);
-  }
-
-  _onError(var e) {
+  
+  onError(e) {
     print(e);
   }
 
+  Future<Response> execute2() {
+    var conn = client.openUrl(method, actualUri);
+    return conn.then(prepareRequest).then(onResponse);
+  }
+  
+  Future<Response> execute([HttpClient perRequestClient]) => 
+    client.openUrl(method, actualUri)
+      .then(prepareRequest, onError: onError)
+      .then(onResponse, onError: onError);
 }
 
 class Response {
 
-  Request request;
-  HttpClientResponse raw;
-
-  final _bodyBuffer = new StringBuffer();
-
-  final Completer<Response> _finished = new Completer<Response>();
-
-  Response();
-
-  HttpHeaders get headers => raw.headers;
-  List<Cookie> get cookies => raw.cookies;
-  int get statusCode => raw.statusCode;
-
-  _bind(request, raw) {
-    this.request = request;
-    this.raw = raw;
-    if (request.stream) {
-      _finished.complete(this);
-    }
-    else {
-      raw.inputStream.onData = _onData;
-      raw.inputStream.onClosed = _onClosed;
-    }
-
+  final HttpClientResponse raw;
+  final Completer finished = new Completer();
+  
+  String _content;
+  
+  Response(this.raw) {
+    final b = new StringBuffer();
+    stringStream.listen(
+        (data) => b.write(data),
+        onDone: () {
+          _content = b.toString();
+          finished.complete(this);
+        }
+    );
   }
-
-  _onData() {
-    var data = raw.inputStream.read(1024);
-    data.forEach((c) => _bodyBuffer.addCharCode(c));
-  }
-
-  _onClosed() {
-    _finished.complete(this);
-  }
-
-  String get content {
-    return _bodyBuffer.toString();
-  }
-
-  dynamic get json {
-    return JSON.parse(content);
-  }
+  
+  Stream<String> get stringStream => new StringDecoder().bind(raw);
+  
+  String get content => _content;
+  
+  dynamic get json => JSON.parse(content);
 
 }
 
-Map<String, String> splitQueryString(String queryString) {
-    Map<String, String> result = new Map<String, String>();
-    int currentPosition = 0;
-    while (currentPosition < queryString.length) {
-      int position = queryString.indexOf("=", currentPosition);
-      if (position == -1) {
-        break;
-      }
-      String name = queryString.substring(currentPosition, position);
-      currentPosition = position + 1;
-      position = queryString.indexOf("&", currentPosition);
-      String value;
-      if (position == -1) {
-        value = queryString.substring(currentPosition);
-        currentPosition = queryString.length;
-      } else {
-        value = queryString.substring(currentPosition, position);
-        currentPosition = position + 1;
-      }
-      result[decodeUriComponent(name)] = decodeUriComponent(value);
-    }
-    return result;
-  }
+//class _Response {
+//
+//  Request request;
+//  HttpClientResponse raw;
+//
+//  final _bodyBuffer = new StringBuffer();
+//
+//  final Completer<Response> _finished = new Completer<Response>();
+//
+//  Response();
+//
+//  HttpHeaders get headers => raw.headers;
+//  List<Cookie> get cookies => raw.cookies;
+//  int get statusCode => raw.statusCode;
+//
+//  _bind(request, raw) {
+//    this.request = request;
+//    this.raw = raw;
+//    if (request.stream) {
+//      _finished.complete(this);
+//    }
+//    else {
+//      raw.inputStream.onData = _onData;
+//      raw.inputStream.onClosed = _onClosed;
+//    }
+//
+//  }
+//
+//  _onData() {
+//    var data = raw.inputStream.read(1024);
+//    data.forEach((c) => _bodyBuffer.addCharCode(c));
+//  }
+//
+//  _onClosed() {
+//    _finished.complete(this);
+//  }
+//
+//  String get content {
+//    return _bodyBuffer.toString();
+//  }
+//
+//  dynamic get json {
+//    return JSON.parse(content);
+//  }
+//
+//}
 
-String buildQueryString(Map<String, String> data) {
-  return Strings.join(data.keys.map((k) {
-    return '${encodeUriComponent(k)}=${encodeUriComponent(data[k])}';
-  }), "&");
-}
 
 Uri mergeArgs(Uri original, Map<String, String> args) {
-  Map originalArgs = splitQueryString(original.query);
+  Map oldArgs = original.queryParameters;
   Map newArgs = new Map.from(args);
-  originalArgs.forEach((k, v) => newArgs[k] = v);
-  return new Uri.fromComponents
-     ( scheme   : original.scheme
-     , userInfo : original.userInfo
-     , domain   : original.domain
-     , port     : original.port
-     , path     : original.path
-     , query    : buildQueryString(newArgs)
-     , fragment : original.fragment
-     );
+  oldArgs.forEach((k, v) => newArgs[k] = v);
+  print(newArgs);
+  return new Uri
+    ( scheme          : original.scheme
+    , userInfo        : original.userInfo
+    , host            : original.host
+    , port            : original.port
+    , path            : original.path
+    , queryParameters : newArgs
+    , fragment        : original.fragment
+    );
 }
 
 Future<Response> request
     ( method
-    , url
+    , uri
     ,
-    { Map<String, String> params
-    , Data data
-    , Map<String, String> headers
+    { Map<String, String> params : const <String, String>{}
+    , Content data
+    , Map<String, String> headers 
     , List<Cookie> cookies
     , List<String> parts
-    , List<InputStream> partStreams
     }) {
-
-  var r = new Request( method, url
-                     , params: params
-                     , data: data
-                     , headers: headers
-                     , cookies: cookies
-                     , parts: parts
-                     , partStreams: partStreams
-                     );
+  Request r = new Request();
+  r.method = method;
+  if (uri is String) {
+    uri = Uri.parse(uri);
+  }
+  r.uri = uri;
+  r.params = params;
+  r.data = data;
+  r.headers = headers;
+  r.cookies = cookies;
   return r.execute();
 }
 
 main() {
-  SecureSocket.initialize();
   var u = 'https://www.googleapis.com/discovery/v1/apis/urlshortener/v1/rest';
-  //request('GET', u).then((resp) => print(resp.json));
-  request('POST', 'http://localhost:8000',
-      data : new JsonData({'a':'b'})).then((resp) => print(resp.content));
+  request('GET', u, params: {'prettyprint': 'true'}).then((Response resp) {
+    print(resp.json);
+  });
 }
